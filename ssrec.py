d3664b50124f1c1bd2f56e367f1507ac822ac3ae
Neil Aspinall <mail@neilaspinall.co.uk>
2016-09-06 13:43:07 +0100

    Add check for duplicate part transactions

--- ssrec.py
+++ ssrec.py
@@ -6,23 +6,45 @@ from datetime import date, datetime, timedelta
 from itertools import combinations
 from StringIO import StringIO
 
+TYPE_FUND = "Loan part fund"
+TYPE_SALE = "Loan part sale"
+TYPE_INTEREST = "Interest"
+TYPE_CAPITAL = "Capital repayment"
+TYPE_DEPOSIT = "Deposit"
+TYPE_WITHDRAWAL = "Withdrawal"
+TYPE_OPENING = "Opening Balance"
+TYPE_AVAILABLE = "Available Balance"
+TYPE_CREDIT = "Affiliate credit"
+
+ATTR_PARTID = "Id"
+ATTR_PARTAMOUNT = "Amount"
+ATTR_START_DATE = "Start Date"
+ATTR_END_DATE = "End Date"
+ATTR_DETAILS = "Asset Details"
+
+ATTR_TXID = "Loan part ID"
+ATTR_TXTYPE = "Transaction type"
+ATTR_TXDATE = "Txn Date"
+ATTR_TXAMOUNT = "Txn Amount"
+ATTR_BALANCE = "Balance"
+
 def getInterestExpected(todayDate, partData):
     yesterday = todayDate - timedelta(1)
     fomDate = yesterday.replace(day=1)
     intDict = {}
     for part in partData:
-        id = int(part["Id"])
-        startDateOrig = datetime.strptime(part["Start Date"], "%d/%m/%Y").date()
+        id = int(part[ATTR_PARTID])
+        startDateOrig = datetime.strptime(part[ATTR_START_DATE], "%d/%m/%Y").date()
         startDate = max(startDateOrig, fomDate)
         endDate = todayDate
-        if part["End Date"]:
-            endDateOrig = datetime.strptime(part["End Date"], "%d/%m/%Y").date()
+        if part[ATTR_END_DATE]:
+            endDateOrig = datetime.strptime(part[ATTR_END_DATE], "%d/%m/%Y").date()
             if endDateOrig <= todayDate:
                 endDate = endDateOrig
         # huh?
         bonusDays = 1 if startDateOrig < fomDate and endDate < todayDate else 0
         if endDate > fomDate and startDate < todayDate:
-            intDict[id] = ((((endDate - startDate).days + bonusDays) / 365.) * 0.12) * locale.atof(part["Amount"])
+            intDict[id] = ((((endDate - startDate).days + bonusDays) / 365.) * 0.12) * locale.atof(part[ATTR_PARTAMOUNT])
     return intDict
 
 def getInterestActual(todayDate, txData):
@@ -30,12 +52,12 @@ def getInterestActual(todayDate, txData):
     fomDate = yesterday.replace(day=1)
     intDict = {}
     for part in txData:
-        if not part["Transaction type"] == "Interest" or not part["Txn Date"] or not part["Loan part ID"]:
+        if not part[ATTR_TXTYPE] == TYPE_INTEREST or not part[ATTR_TXDATE] or not part[ATTR_TXID]:
             continue
-        txnDate = datetime.strptime(part["Txn Date"], "%d/%m/%Y").date()
-        id = int(part["Loan part ID"])
+        txnDate = datetime.strptime(part[ATTR_TXDATE], "%d/%m/%Y").date()
+        id = int(part[ATTR_TXID])
         if txnDate < todayDate and txnDate >= fomDate:
-            intDict[id] = float(part["Txn Amount"])
+            intDict[id] = float(part[ATTR_TXAMOUNT])
     return intDict
 
 def getRecombinedInterest(recDate, partDataOrig, txData):
@@ -45,15 +67,15 @@ def getRecombinedInterest(recDate, partDataOrig, txData):
     if sum(expected.values()) or sum(actual.values()):
         diffs = dict([(i, actual[i] - expected[i]) for i in expected if i in actual and abs(round(actual[i] - expected[i], 2)) > 0])
 
-        partData = dict([(int(p["Id"]), p) for p in partDataOrig])
-        fundData = dict([(int(p["Loan part ID"]), p) for p in txData if p["Transaction type"] == "Loan part fund"])
+        partData = dict([(int(p[ATTR_PARTID]), p) for p in partDataOrig])
+        fundData = dict([(int(p[ATTR_TXID]), p) for p in txData if p[ATTR_TXTYPE] == TYPE_FUND])
         combo = {}
         for d in diffs:
-            origSize = abs(locale.atof(fundData[d]["Txn Amount"]))
-            currSize = locale.atof(partData[d]["Amount"])
-            loanName = partData[d]["Asset Details"]
-            startDate = partData[d]["Start Date"]
-            childAmounts = dict([(c, locale.atof(partData[c]["Amount"])) for c in partData if c not in fundData and c != d and partData[c]["Asset Details"] == loanName and partData[c]["Start Date"] == startDate])
+            origSize = abs(locale.atof(fundData[d][ATTR_TXAMOUNT]))
+            currSize = locale.atof(partData[d][ATTR_PARTAMOUNT])
+            loanName = partData[d][ATTR_DETAILS]
+            startDate = partData[d][ATTR_START_DATE]
+            childAmounts = dict([(c, locale.atof(partData[c][ATTR_PARTAMOUNT])) for c in partData if c not in fundData and c != d and partData[c][ATTR_DETAILS] == loanName and partData[c][ATTR_START_DATE] == startDate])
             i = 1
             while i <= len(childAmounts):
                 for c in combinations(childAmounts.items(), i):
@@ -77,15 +99,27 @@ def getRecombinedInterest(recDate, partDataOrig, txData):
 
     return expected, actual
 
-def getTransactionTotal(txData, txType, valueType="Txn Amount"):
+def getTransactionTotal(txData, txType, valueType=ATTR_TXAMOUNT):
     txTotal = 0
     for part in txData:
-        if part["Transaction type"] == txType:
+        if part[ATTR_TXTYPE] == txType:
             txTotal += locale.atof(part[valueType])
     return txTotal
+    
+def checkDuplicates(txData, txType):
+    x = [part[ATTR_TXID] for part in txData if part[ATTR_TXTYPE] == txType]
+    y = set([i for i in x if x.count(i) > 1])
+    return y
 
+def checkAllDuplicates(txData):
+    dupeResults = {}
+    for dupeType in [TYPE_SALE, TYPE_FUND, TYPE_CAPITAL]:
+        dupes = checkDuplicates(txData, dupeType)
+        dupeResults[dupeType] = dupes
+    return dupeResults
+    
 def main():
-    locale.setlocale(locale.LC_ALL, '' if os.name == "nt" else 'en_GB')
+    locale.setlocale(locale.LC_ALL, "" if os.name == "nt" else "en_GB")
     partFile = sys.argv[1]
     txFile = sys.argv[2]
     with open(partFile, "r") as f:
@@ -112,14 +146,22 @@ def main():
         expectedLTD += expectedTotal
         actualLTD += actualTotal
         recDate = (recDate - timedelta(1)).replace(day=1)
-    print "\nSUMMARY:"
+    print
     print "Expected total interest: %8.2f" % expectedLTD
     print "Actual total interest  : %8.2f" % actualLTD
-    cashTotal = getTransactionTotal(txData, "Deposit") + getTransactionTotal(txData, "Withdrawal") + getTransactionTotal(txData, 'Affiliate credit')
-    tradeTotal = getTransactionTotal(txData, "Capital repayment") + getTransactionTotal(txData, "Loan part sale") + getTransactionTotal(txData, "Loan part fund") 
-    openBalance = getTransactionTotal(txData, "Opening Balance", "Balance")
-    currBalance = getTransactionTotal(txData, "Available Balance", "Balance")
-    print "\nBalance (expected interest + actual txn): %8.2f" % (openBalance + cashTotal + expectedLTD + tradeTotal)
+    print
+    dupeResults = checkAllDuplicates(txData)
+    for dupeType, dupes in dupeResults.items():
+        if dupes:
+            print "Duplicate %ss: %s" % (dupeType.lower(), ", ".join(dupes))
+        else:
+            print "No duplicate %ss" % dupeType.lower()
+    cashTotal = getTransactionTotal(txData, TYPE_DEPOSIT) + getTransactionTotal(txData, TYPE_WITHDRAWAL) + getTransactionTotal(txData, TYPE_CREDIT)
+    tradeTotal = getTransactionTotal(txData, TYPE_CAPITAL) + getTransactionTotal(txData, TYPE_SALE) + getTransactionTotal(txData, TYPE_FUND) 
+    openBalance = getTransactionTotal(txData, TYPE_OPENING, ATTR_BALANCE)
+    currBalance = getTransactionTotal(txData, TYPE_AVAILABLE, ATTR_BALANCE)
+    print
+    print "Balance (expected interest + actual txn): %8.2f" % (openBalance + cashTotal + expectedLTD + tradeTotal)
     print "Balance (actual interest + actual txn)  : %8.2f" % (openBalance + cashTotal + actualLTD + tradeTotal)
     print "Statement balance                       : %8.2f" % currBalance
 
